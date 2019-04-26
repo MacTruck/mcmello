d0d57f817add576f59f80babd68414557e8444e8
Scott McInnis <scottalyst@gmail.com>
2019-04-25 19:40:38 -0700

    added createServer

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env nodejs
 var http = require('http');
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
 }).listen(8080, 'localhost');
-console.log('Server running');
+console.log('Server running at http://localhost:8080/');
 
 var express = require('express');
 var bodyParser = require('body-parser');
