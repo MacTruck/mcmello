a7035f6f026c61653f69fcd5b8c8559d75c1c5be
Scott McInnis <scottalyst@gmail.com>
2019-04-25 18:59:00 -0700

    branching macinations into mcmello

---
+++ assets/macinations.js
@@ -0,0 +1,135 @@
+// functionality for hero slide header icon
+		var items = document.querySelectorAll(".hero article");
+		var header = document.querySelector("header");
+		var hero = document.querySelector(".hero");
+		var icon = document.querySelector("header .icon");
+			var iconTop = document.querySelectorAll(".iconTop");
+			var iconMiddle = document.querySelectorAll(".iconMiddle");
+			var iconBottom = document.querySelectorAll(".iconBottom");
+			var iconDot = document.querySelectorAll(".iconDot");
+		var backArrow = document.querySelector("#backArrow");
+		var bottomArrow = document.querySelector("#bottomArrow");
+		var title = document.querySelector(".title");
+		var clicked = false;
+		var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+// add id's to printContainers
+		var printContainers = document.querySelectorAll(".printContainer");
+		var printContainerCounter = 1;
+		printContainers.forEach(printContainer => {
+			printContainer.id = `printContainer${printContainerCounter}`;
+			// console.log(printContainer.id);
+			printContainerCounter += 1;
+		});
+
+// change icon color on hover
+		icon.addEventListener("mouseover", () => {
+			iconTop.forEach(path => path.style.fill = "var(--green3)");
+			iconMiddle.forEach(path => path.style.fill = "var(--green2)");
+			iconBottom.forEach(path => path.style.fill = "var(--green1)");
+			iconDot.forEach(path => path.style.fill = "var(--green4)");
+		});
+	// change icon color back off hover
+		icon.addEventListener("mouseleave", () => {
+			iconTop.forEach(path => path.style.fill = "var(--purple3)");
+			iconMiddle.forEach(path => path.style.fill = "var(--purple2)");
+			iconBottom.forEach(path => path.style.fill = "var(--purple1)");
+			iconDot.forEach(path => path.style.fill = "var(--purple4)");
+		});
+
+		function addListeners() {
+			items.forEach(item => {
+				var itemName = item.id;
+				var itemProject = document.getElementById(`${itemName}Project`);
+// set background image of hero as articles on hovered
+				item.addEventListener("mouseover", () => {
+					if (clicked == false) {
+						hero.style.background = `url("assets/images/${itemName}.webp")`;
+						hero.style.backgroundSize = "cover";
+						hero.style.backgroundRepeat = "no-repeat";
+						hero.style.backgroundPosition = "center";
+					}
+				});
+	// reset background off hover
+				item.addEventListener("mouseleave", () => {
+					if (clicked == false) { hero.style.background = "#191919"; }
+				});
+// if clicked move hero to the left and open clicked article
+				item.addEventListener("click", () => {
+					hero.style.position = "absolute";
+					hero.style.backgroundImage = `url("assets/images/${itemName}.webp")`;
+					header.scrollIntoView();
+					hero.style.transform = "translateX(-100%)";
+					itemProject.classList.add("active");
+					backArrow.style.left = "0";
+					clicked = true;
+// check if itemProject contains a slideBox, sets up slideBox if true
+				});
+				if (itemProject.contains(itemProject.querySelector(".slideBox"))) { 
+					var slideBoxes = itemProject.querySelectorAll(".slideBox");
+					slideBoxes.forEach(slideBox => {
+						var slides = slideBox.querySelectorAll(".slide");
+				// initialize slideBox defaults	
+						if (slideBox.querySelectorAll(".showSlide").length == 0) {
+							var slideCount = 0;
+							var slidesTotal = slides.length;
+							var currentSlide = slides[slideCount];
+							currentSlide.classList.add("showSlide");
+						}
+				// toggle next slide display
+						var onNextButtonHandler = function() {
+							currentSlide.classList.remove("showSlide");
+							slideCount += 1;
+							slideCount = slideCount == slidesTotal ? 0 : slideCount;
+							currentSlide = slides[slideCount];
+							currentSlide.classList.add("showSlide");
+							console.log(`slideCount: ${slideCount}`);
+						}
+				// toggle previous slide display
+						var onPreviousButtonHandler = function() {
+							currentSlide.classList.remove("showSlide");
+							slideCount -= 1;
+							slideCount = slideCount < 0 ? (slidesTotal - 1) : slideCount;
+							currentSlide = slides[slideCount];
+							currentSlide.classList.add("showSlide");
+						}
+				// setup eventlisteners for next/previous buttons
+						var nextButton = slideBox.querySelector(".next");
+						nextButton.addEventListener("click", onNextButtonHandler, false);
+						var previousButton = slideBox.querySelector(".previous");
+						previousButton.addEventListener("click", onPreviousButtonHandler, false);
+					});
+				}
+			});
+// backArrow clicked, remove article and move hero back into position
+			backArrow.addEventListener("click", () => {
+				hero.style.position = "relative";
+				hero.style.transform = "translateX(0)";
+				var currentItem = document.querySelector(".active");
+				currentItem.classList.toggle("active");
+				backArrow.style.left = "-2em";
+				header.scrollIntoView();
+				clicked = false;
+			});
+// bottomArrow clicked (for prints page), advances to next print
+			bottomArrow.addEventListener("click", () => {
+				var currentContainer = 0;
+	// determine first printContainer with midpoint in viewport
+				printContainers.forEach(printContainer => {
+					var position = printContainer.getBoundingClientRect();
+					var midpoint = (position.bottom - position.top) / 2 + position.top;
+					if (midpoint >= 0 && currentContainer == 0) {
+						currentContainer = Number(printContainer.id.match(/\d+/g));
+					}
+				});
+	// select and scroll to midpoint of next printContainer
+				var nextContainer = currentContainer + 1 < printContainerCounter ? currentContainer + 1 : 1;
+				var nextPrintContainer = document.querySelector(`#printContainer${nextContainer}`);
+				var nextPosition = nextPrintContainer.getBoundingClientRect();
+				var nextMidpoint = (nextPosition.bottom - nextPosition.top) / 2 + nextPosition.top;
+				var nextTop = nextMidpoint - (windowHeight / 2);
+				window.scrollBy(0, nextTop);
+			});
+		}
+
+		addListeners();
---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var jsonParser = bodyParser.json();
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.listen(5000);
+
+app.get('/', function(req, res) {
+	console.log("where are you?");
+	res.render('index');
+});
+
+app.get('/art', function(req, res) {
+	res.render('art');
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+app.get('/inquire', function(req, res) {
+	res.render('inquire');
+});
+
+app.post('/inquire', jsonParser, function(req, res) {
+	console.log(req.body);
+	res.render('inquire-success', {data: req.body});
+});
+
+console.log("Supposedly running...");
