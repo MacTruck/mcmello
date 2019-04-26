0702c34a695f7da21dc2d79c307f0aefd0db688e
Scott McInnis <scottalyst@gmail.com>
2019-04-25 20:02:51 -0700

    express version update

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, '206.189.218.131');
-console.log('Server running at http://localhost:8080/');
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -14,9 +6,14 @@ var jsonParser = bodyParser.json();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
+app.listen(5000, function () {
+	console.log('App listening on port 5000');
+});
+
 app.get('/', function(req, res) {
 	console.log("where are you?");
-	res.render('index');
+	res.send('Hello there');
+	// res.render('index');
 });
 
 app.get('/art', function(req, res) {
