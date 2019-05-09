336673039c705b66c65fdcb1a4c979ecd7ecdb18
Scott McInnis <scottalyst@gmail.com>
2019-05-09 15:53:19 -0700

    adjusted lazy loading settings

--- public/assets/mcmello.js
+++ public/assets/mcmello.js
@@ -1,58 +1,3 @@
-// lazyload images for print page
-		document.addEventListener("DOMContentLoaded", function() {
-			var lazyloadImages;
-
-			if ("IntersectionObserver" in window) {
-				lazyloadImages = document.querySelectorAll(".lazy");
-				var imageObserver = new IntersectionObserver(function(entries, observer) {
-					entries.forEach(function(entry) {
-						if (entry.isIntersecting) {
-							var image = entry.target;
-							image.src = image.dataset.src
-							image.classList.remove("lazy");
-							image.unobserve(image);
-						}
-					});
-				});
-
-				lazyloadImages.forEach(function(image) {
-					imageObserver.observe(image);
-				});
-			} else {
-				var lazyloadThrottleTimeout;
-				lazyloadImages = document.querySelectorAll(".lazy");
-
-				function lazyload() {
-					if (lazyloadThrottleTimeout) {
-						clearTimeout(lazyloadThrottleTimeout);
-					}
-
-					lazyloadThrottleTimeout = setTimeout(function() {
-						var scrollTop = window.pageYOffset;
-
-						lazyloadImages.forEach(function(img) {
-							if(img.offsetTop < (window.innerHeight + scrollTop)) {
-								img.src = img.dataset.src;
-								img.classList.remove("lazy");
-							}
-						});
-
-						if (lazyloadImages.length == 0) {
-							document.removeEventListener("scroll", lazyload);
-							window.removeEventListener("resize", lazyload);
-							window.removeEventListener("orientation", lazyload);
-						}
-					}, 20);
-				}
-
-				document.addEventListener("scroll", lazyload);
-				window.addEventListener("resize", lazyload);
-				window.addEventListener("orientation", lazyload);
-			}
-		});
-
-
-
 // functionality for hero slide header icon
 		var items = document.querySelectorAll(".hero article");
 		var header = document.querySelector("header");
@@ -191,5 +136,58 @@
 				});
 			}
 		}
+
+// lazyload images for print page
+		document.addEventListener("DOMContentLoaded", function() {
+			var lazyloadImages;
+
+			if ("IntersectionObserver" in window) {
+				lazyloadImages = document.querySelectorAll(".lazy");
+				var imageObserver = new IntersectionObserver(function(entries, observer) {
+					entries.forEach(function(entry) {
+						if (entry.isIntersecting) {
+							var image = entry.target;
+							image.src = image.dataset.src
+							image.classList.remove("lazy");
+							image.unobserve(image);
+						}
+					});
+				}, { root: null, rootMargin: '0 0 1000px 0'});
+
+				lazyloadImages.forEach(function(image) {
+					imageObserver.observe(image);
+				});
+			} else {
+				var lazyloadThrottleTimeout;
+				lazyloadImages = document.querySelectorAll(".lazy");
+
+				function lazyload() {
+					if (lazyloadThrottleTimeout) {
+						clearTimeout(lazyloadThrottleTimeout);
+					}
+
+					lazyloadThrottleTimeout = setTimeout(function() {
+						var scrollTop = window.pageYOffset;
+
+						lazyloadImages.forEach(function(img) {
+							if(img.offsetTop < (window.innerHeight + scrollTop)) {
+								img.src = img.dataset.src;
+								img.classList.remove("lazy");
+							}
+						});
+
+						if (lazyloadImages.length == 0) {
+							document.removeEventListener("scroll", lazyload);
+							window.removeEventListener("resize", lazyload);
+							window.removeEventListener("orientation", lazyload);
+						}
+					}, 20);
+				}
+
+				document.addEventListener("scroll", lazyload);
+				window.addEventListener("resize", lazyload);
+				window.addEventListener("orientation", lazyload);
+			}
+		});
 
 		addListeners();
